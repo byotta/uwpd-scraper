0aeede82b87a24050e93146c0ee040ed0d09c6d9
byotta <byotta@cs.washington.edu>
2020-09-23 01:12:23 -0700

    added comparison

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@
 Scrapes UWPD recent crime log webpage and sends an email when it updates
 """
 import requests
+import time
 from bs4 import BeautifulSoup
 
 url = "http://police.uw.edu/crimedata/60daylog/"
@@ -11,4 +12,13 @@ while True:
     soup = BeautifulSoup(response.text, "html.parser")
     element = soup.find("iframe", {"class": "uw-pdf-view"})
     firstPDF = element["src"]
-    print(firstPDF)
+
+    time.sleep(300)
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    element = soup.find("iframe", {"class": "uw-pdf-view"})
+    secondPDF = element["src"]
+    if firstPDF != secondPDF:
+        # alert with email
+        print("new crime")
